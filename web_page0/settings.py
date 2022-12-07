0d59c5a7cafd88b0ab65c914f7f1d0ac2fbe9461
nature <nature.kim@gmail.com>
2022-12-07 10:54:45 +0900

    구글 로그인 기능 추가하기

--- web_page0/settings.py
+++ web_page0/settings.py
@@ -45,6 +45,13 @@ INSTALLED_APPS = [
     'crispy_forms',
     'markdownx',
 
+    # 2022.12.06
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+
     'blog',
     'single_pages',
 ]
@@ -131,3 +138,17 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTHENTICATION_BACKENDS = [
+
+    'django.contrib.auth.backends.ModelBackend',
+
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+]
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VARIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/blog/'
